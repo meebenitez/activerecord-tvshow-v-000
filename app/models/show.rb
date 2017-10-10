6e930460204bacd8c62393a48e31e1039b30f4ca
Mary Benitez <meebenitez@gmail.com>
2017-10-10 03:21:53 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,12 +1,16 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    show = Show.maximum(:rating)
+    Show.maximum(:rating)
   end
 
   def self.most_popular_show
     result = Show.highest_rating
     show = Show.find_by(rating: "#{result}")
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
   end
 
 
