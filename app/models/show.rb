a08b244b2aa67fc7966cb8ac7f0c829406e2c9bb
Mary Benitez <meebenitez@gmail.com>
2017-10-10 03:18:21 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,12 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    Show.maximum(:rating)
+    show = Show.maximum(:rating)
+    binding.pry
   end
 
   def self.most_popular_show
-    
+
   end
 
 
